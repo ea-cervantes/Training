70f22abe3a2134edccbdd852693b2d9867b6c196
ea-cervantes <e.cervantes@caci.com>
2022-06-15 14:23:12 -0600

    Bash loop 2

--- DirLoop.sh
+++ DirLoop.sh
@@ -1,9 +1,19 @@
 #! /usr/bin/bash
 initDir=$(pwd)
 echo $initDir
+if [ -f "DirLoopOut.txt" ]; then
+    rm DirLoopOut.txt
+fi
 touch DirLoopOut.txt
+
 cd /
 # find . -maxdepth 1 > $initDir/DirLoopOut.txt
-if
+for FILE in *; do
+if [ -d "$FILE" ];
+then
+    echo $FILE >> $initDir/DirLoopOut.txt
+fi
+done
 
 cd $initDir
+cat DirLoopOut.txt
