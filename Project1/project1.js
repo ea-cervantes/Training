dc81b4077bb195ba0a792374de87be2c8090c558
ea-cervantes <e.cervantes@caci.com>
2022-06-17 09:37:39 -0600

    Project 1 update 2

--- Project1/project1.js
+++ Project1/project1.js
@@ -6,9 +6,14 @@ let borderColor = document.getElementById("border-color")
 let paddingColor = document.getElementById("padding-color")
 let contentColor = document.getElementById("content-color")
 
+let borderRadius = document.getElementById("border-radius")
+let paddingRadius = document.getElementById("padding-radius")
+let contentRadius = document.getElementById("content-radius")
+
 let borderBox = document.querySelector(".border")
 let paddingBox = document.querySelector(".padding")
 let contentBox = document.querySelector(".content")
+
 
 borderRange.addEventListener("change",function(){
     borderBox.style.padding = borderRange.value + "px"
@@ -20,6 +25,7 @@ contentRange.addEventListener("change",function(){
     contentBox.style.padding = contentRange.value + "px"
 })
 
+
 borderColor.addEventListener("change",function(){
     borderBox.style.backgroundColor = borderColor.value
 })
@@ -28,4 +34,14 @@ paddingColor.addEventListener("change",function(){
 })
 contentColor.addEventListener("change",function(){
     contentBox.style.backgroundColor = contentColor.value
+})
+
+borderRadius.addEventListener("change",function(){
+    borderBox.style.borderRadius = borderRadius.value + "px"
+})
+paddingRadius.addEventListener("change",function(){
+    paddingBox.style.borderRadius = paddingRadius.value + "px"
+})
+contentRadius.addEventListener("change",function(){
+    contentBox.style.borderRadius = contentRadius.value + "px"
 })
